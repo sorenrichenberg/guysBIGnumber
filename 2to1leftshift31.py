cecb663354f25c67c71808e8bc41ee6d6c1afaf9
Soren Richenberg <sorensrichenberg@gmail.com>
2020-10-07 17:10:00 -0700

    added intermediary numbers and additional output file

--- 2to1leftshift31.py
+++
@@ -1,8 +0,0 @@
-def guysBIGnumber(n):
-    return 2 ** (1 << n)
-
-def main():
-    print(guysBIGnumber(31))
-
-if __name__ == "__main__":
-    main()
---
+++ from_one_to_1_left_31.py
@@ -0,0 +1,28 @@
+import sys
+
+def from_one_to_1_left_31(output_file_name, sum_file_name):
+    output_file = open(output_file_name, 'w')  
+
+    sum = 0
+    for i in range(1 << 31):
+        num = 1 << i
+        print(num)
+        output_file.write(str(num) + '\n')
+        sum += num
+
+    output_file.close() 
+
+    sum_file = open(sum_file_name, 'w')
+    sum_file.write("sum:\n" + str(sum) + "\n")
+    sum_file.close()
+
+def main():
+    from_one_to_1_left_31(sys.argv[1], sys.argv[2])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("usage: \n$ python output_file.txt sum_file.txt")
+        sys.exit(1)
+
+    main()
+
