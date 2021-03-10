444a7c3c9e4e66d76461df26ab1632064faec678
Soren Richenberg <sorensrichenberg@gmail.com>
2021-03-09 19:17:45 -0800

    format

--- from_one_to_1_left_31.py
+++ from_one_to_1_left_31.py
@@ -1,16 +1,16 @@
 import sys
 
-def from_one_to_1_left_31(output_file_name, sum_file_name):
-    output_file = open(output_file_name, 'w')  
+def from_one_to_1_left_31(sequence_file_name, sum_file_name):
+    sequence_file = open(sequence_file_name, 'w')  
 
     sum = 0
     for i in range(1 << 31):
         num = 1 << i
         print(num)
-        output_file.write(str(num) + '\n')
+        sequence_file.write(str(num) + '\n')
         sum += num
 
-    output_file.close() 
+    sequence_file.close() 
 
     sum_file = open(sum_file_name, 'w')
     sum_file.write("sum:\n" + str(sum) + "\n")
@@ -18,7 +18,7 @@ def from_one_to_1_left_31(output_file_name, sum_file_name):
 
 def main():
     if len(sys.argv) != 3:
-        print("usage: \n$ python output_file.txt sum_file.txt")
+        print("usage: \n$ python sequence_file.txt sum_file.txt")
         sys.exit(1)
 
     from_one_to_1_left_31(sys.argv[1], sys.argv[2])
